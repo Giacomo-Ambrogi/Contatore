42752ca3d694a85c74a629b85681ea1881782b8c
Giacomo-Ambrogi <giacky597@gmail.com>
2025-11-17 12:47:41 +0100

    aricamento progetto completo

---
+++ src/ContatoreSyinchronized.java
@@ -0,0 +1,14 @@
+public class ContatoreSyinchronized {
+    private final Object lock = new Object();
+    private int count = 0;
+
+    public void increment() {
+        synchronized (lock) {
+            count++;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        //main senza classe contatore sincronizzata
+        Contatore c = new Contatore();
+        Thread t1 = new Thread(() -> c.increment());
+        Thread t2 = new Thread(() -> c.increment());
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Conteggio finale: " + c.getCount());
+
+        //main con classe contatore sincronizzata
+        ContatoreSyinchronized contSync = new ContatoreSyinchronized();
+
+        // Create multiple threads to increment the counter
+        Thread t3 = new Thread(() -> contSync.increment());
+        Thread t4 = new Thread(() -> contSync.increment());
+
+        t3.start();
+        t4.start();
+
+        try {
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Conteggio finale synchronized: " + contSync.getCount());
     }
 }
